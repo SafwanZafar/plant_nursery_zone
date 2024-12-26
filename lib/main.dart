8d27a7089f7bada47b7b6d8fb9b3a8b6e5d1a56c
Usama Umer <usamaumer831@gmail.com>
2024-12-26 13:31:24 +0500

    update main, added provider_dependency

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:plant_nursery_zone/admin/admin_dashboard.dart';
 import 'package:plant_nursery_zone/authentication/login.dart';
 import 'package:plant_nursery_zone/authentication/register.dart';
+import 'package:plant_nursery_zone/model/cart_model.dart';
+import 'package:plant_nursery_zone/customer/home.dart';
+import 'package:plant_nursery_zone/customer/shopping_cart.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +17,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ChangeNotifierProvider(create: (context)=>CartModel(),
+      child:MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: AdminDashboard(),
+      home: Home(),
+    ),
     );
   }
 }
